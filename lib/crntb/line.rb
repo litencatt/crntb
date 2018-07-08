bce4b4a3f90d2d7b142569529dcd6449c3be10b1
Nakamura Kosuke <ncl0709@gmail.com>
2018-07-08 14:33:37 +0900

    Split to other file

--- lib/crntb/line.rb
+++ lib/crntb/line.rb
@@ -2,9 +2,7 @@ module Crntb
   class Line
     def self.parse(line)
       fields = Fields.new(line)
-
       result = Outputer::Text.build(fields)
-
       %Q{#{result}\n  run command "#{fields.command}"}
     end
   end
